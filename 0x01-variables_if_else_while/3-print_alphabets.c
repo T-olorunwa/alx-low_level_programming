fff9a8b6fef18ca48b6bdb54416642edbcafbbbd
Qudus <Aqolorunwa@gmail.com>
2023-03-18 07:54:12 -0700

    Upper and lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,13 +13,15 @@ int main(void)
 	for (x = 'a' ; x <= 'z'; x++)
 {
 	int lower_x = x;
+
 	putchar(lower_x);
 }
-	putchar('\n');
 	for (x = 'A' ; x <= 'Z'; x++)
 {
 	int upper_x = x;
+
 	putchar(upper_x);
 }
+	putchar('\n');
 	return (0);
 }
