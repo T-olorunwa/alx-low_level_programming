7b6e417c41fd2b303a201ecb5c83981185d4ebb2
Qudus <Aqolorunwa@gmail.com>
2023-03-18 01:54:48 -0700

    +ve_or_-ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+void sample_func(int)
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
+int main()
 {
 	int n;
 	
