ef093d14da04295af0a3dd0cb806ae769cb81c8d
Qudus <Aqolorunwa@gmail.com>
2023-03-18 04:07:14 -0700

    lover and upper case letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    
+    for(int x = 'A' ; x <= 'Z'; x++)
+{
+    int lower_x = tolower(x);
+    putchar(lower_x);
+}
+    putchar('\n');
+    for(int x = 'A' ; x <= 'Z'; x++)
+{
+    int upper_x = x;
+    putchar(upper_x);
+}
+    return (0);
+}
