8db2224df302c2bf4d06842be8af835fb091fb78
Qudus <Aqolorunwa@gmail.com>
2023-03-18 12:10:47 -0700

    without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+    char x;
+    char q;
+    char e;
+    
+    for (x = 'a'; x <= 'z'; x++)
+    {
+        if (x != 'q' && x != 'e')
+        putchar(x);
+    }
+    putchar('\n');
+    
+    return 0;
+}
