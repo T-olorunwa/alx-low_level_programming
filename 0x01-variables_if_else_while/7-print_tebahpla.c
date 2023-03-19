94758a64a14662304cc5737fcbf387c46bcec733
Qudus <Aqolorunwa@gmail.com>
2023-03-19 04:24:43 -0700

    reverse_alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    int x;
-    
-    for (x = 'z'; x >= 'a'; x--)
-    putchar(x);
-    
-    putchar('\n');
-    
-    return (0);
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	putchar(x);
+
+	putchar('\n');
+
+	return (0);
 }
