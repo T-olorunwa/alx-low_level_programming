146f59a02534ea560e0a561f0f670d785ef8b4d5
Qudus <Aqolorunwa@gmail.com>
2023-03-19 04:08:36 -0700

    Decimal using putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for(x = '0'; x <= '9'; x++)
+{
+		putchar(x);
+}
+
+	putchar('\n');
+	return (0);
+}
