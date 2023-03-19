b6ee7d548f175dbbcb02cf81b5c620bbcba11863
Qudus <Aqolorunwa@gmail.com>
2023-03-19 02:37:46 -0700

    decimal

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  miain - Entry Point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	while (a < 10)
+{
+	printf("%d", a);
+	a++;
+}
+	putchar('\n');
+	return (0);
+}
