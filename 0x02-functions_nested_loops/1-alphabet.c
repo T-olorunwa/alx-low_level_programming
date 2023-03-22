665e3a4f8a4bfa1c81f3296a2fd505c9249ff637
Qudus <Aqolorunwa@gmail.com>
2023-03-22 10:16:12 -0700

    function that prints alphabet in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,12 @@ void print_alphabet(void);
 
 void print_alphabet(void)
 {
-		int x;
+	char x;
 
-		for (x = 'a'; x <= 'z'; x++)
-			int putchar(x);
-
-		putchar('\n');
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
 }
 
