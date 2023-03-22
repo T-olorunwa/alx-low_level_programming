adc1ed55d13b8a68f9cc3d2362345b8b6879ccf4
Qudus <Aqolorunwa@gmail.com>
2023-03-22 12:06:06 -0700

    alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{	while (x <= 10)
+		putchar(x);
+			x++;
+	}
+	putchar('\n');
+}
