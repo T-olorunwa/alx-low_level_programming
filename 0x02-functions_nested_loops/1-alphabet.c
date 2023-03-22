ffc6003b8f448296516772d4f673308308953d25
Qudus <Aqolorunwa@gmail.com>
2023-03-22 05:25:55 -0700

    Print letters of the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-
-/**
- *  main - Entry point
- *
- *  Return: Always 0 (Success)
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
  */
 
-void alpha(void)
-{
-	int x;
+#include "holberton.h"
 
-	for (x = 'a'; x <= 'z'; x++)
-		_putchar(x);
-		
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }
-
