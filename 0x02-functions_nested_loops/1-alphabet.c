33e97f17d599f47338f38be8c8bab5d6f062741f
Qudus <Aqolorunwa@gmail.com>
2023-03-22 04:53:12 -0700

    Print letters of the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha()
+	{
+		int x;
+
+		for (x = 'a'; x <= 'z'; x++)
+			putchar(x);
+			putchar('\n')
+	}
+
+	return alpha();
+	return (0);
+}
