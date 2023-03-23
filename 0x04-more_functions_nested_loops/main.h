cf47916123ba28b4513d3ea654f47a37acdbeada
Qudus <Aqolorunwa@gmail.com>
2023-03-23 14:43:44 -0700

    is_upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: main.h
+ * Auth: Qudus
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
