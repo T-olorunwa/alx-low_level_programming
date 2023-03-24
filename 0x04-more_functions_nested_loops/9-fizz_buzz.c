e507e963127d5604251c53c1bf76d7332f97baf1
Qudus <Aqolorunwa@gmail.com>
2023-03-24 15:41:10 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x ++)
+	{
+		if ((x % 3) == 0)
+			printf("Fizz " );
+		else if ((x % 5) == 0)
+			printf("Buzz ");
+		else if ((x % 15) == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", x);
+	}
+	return (0);
+}
