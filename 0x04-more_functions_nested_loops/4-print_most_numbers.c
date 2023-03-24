4daa6a0470ba04c5f21a8cf3da3d11f3431c7964
Qudus <Aqolorunwa@gmail.com>
2023-03-24 12:26:06 -0700

    Prints nubers excluding 2 && 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9, followed by a new line
- * do not print 2 and 4.
+ * print_most_numbers - prints the numbers from 0-9 except for 2 and 4.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
