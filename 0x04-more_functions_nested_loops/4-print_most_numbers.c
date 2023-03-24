fdba105c9cf46b6f7b155ee4e0e3ebe1216c955b
Qudus <Aqolorunwa@gmail.com>
2023-03-24 12:20:53 -0700

    Prints nubers excluding 2 && 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9, followed by a new line
+ * print_most_numbers - prints numbers from 0 to 9, followed by a new line
  * do not print 2 and 4.
  */
 
@@ -12,11 +12,7 @@ void print_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
-		
-			continue;
-		else
-			_putchar(num + '0');
-
+			_putchar((num % 10) + '0');
 	}
 
 	_putchar('\n');
