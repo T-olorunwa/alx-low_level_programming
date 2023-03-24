22e0ac23a2fe5242a3adfc6264dfb507aee79ac7
Qudus <Aqolorunwa@gmail.com>
2023-03-24 04:03:19 -0700

    print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print numbers - prints numbers from 0 to 9, followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	int num; 
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+}
