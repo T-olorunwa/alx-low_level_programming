107258cc16b2e2da69ad2ece3fb2bac5e12e3ca5
Qudus <Aqolorunwa@gmail.com>
2023-03-28 21:40:05 -0700

    Print the length of a sting

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts - prints a chracter to the stdout
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
