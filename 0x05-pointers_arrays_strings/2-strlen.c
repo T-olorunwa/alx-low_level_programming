c9a38941e591b5f713c9fc30f6d52edcacf07b32
Qudus <Aqolorunwa@gmail.com>
2023-03-29 01:57:27 -0700

    Print String Length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include "stdio.h"
+#include <string.h>
 
 /**
  * strlen - prints the length of a string.
  * @s: string to be printed.
  */
 
-int _strlen(char *)
+int _strlen(char *s)
 {
 	char *s;
 
