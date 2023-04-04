aacfc6e0b3d330f3bb8a8ddd0802c2a7ea60c197
Qudus <Aqolorunwa@gmail.com>
2023-04-04 14:18:07 -0700

    set memory to a pointed area

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * memset - fills memory with a constant byte
@@ -12,7 +12,7 @@ include "main.h"
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	*memory = s; /*\*memory isthe pointer to the memory s*/
+	unsigned char *memory = s; /*\*memory isthe pointer to the memory s*/
 
 	for (i = 0; i <= n; i++)
 	{
