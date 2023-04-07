8cfff62e0470e264c8aaf5908c904b74b529670d
Qudus <Aqolorunwa@gmail.com>
2023-04-07 10:55:28 -0700

    Reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * print_rev_recursion - prints a string in reverse
+ * @s: The string to be printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s != '\n')
+		_print_rev_recursion();
+		printf("%c", c);
+}
