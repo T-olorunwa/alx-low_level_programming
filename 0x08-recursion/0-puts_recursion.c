56ebd6d7609f02bbe427bcab7ccbb8fd5b89631a
Qudus <Aqolorunwa@gmail.com>
2023-04-07 10:37:15 -0700

    Printing with puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * @s: string to be printed
+ * puts_recursion - prints a string with 
+ * 			a new line
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
