bc91769b95a808a6159eabb6543ac786ade6ddfc
Qudus <Aqolorunwa@gmail.com>
2023-04-08 07:11:33 -0700

    Power of an integer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that raises an integer 
+ * 		to another integer
+ * @x: The base of the equation
+ * @y: The power of the equation
+ *
+ * Return: the value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int res = pow(x, y);
+
+	if (y < 0)
+		return (-1);
+	else
+		return (res);
+}
