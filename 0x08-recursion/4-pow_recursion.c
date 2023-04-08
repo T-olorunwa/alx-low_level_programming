8160470e4c0fb19e813d2b7d1493c7685214c796
Qudus <Aqolorunwa@gmail.com>
2023-04-08 09:41:39 -0700

    Power of a string

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,10 +11,14 @@
 
 int _pow_recursion(int x, int y)
 {
-	int res = (int)pow(x, y);
+	int res;
+
+	res = (int)pow(x, y);
 
 	if (y < 0)
 		return (-1);
+	else if (y == 0)
+		return (1);
 	else
 		return (res);
 }
