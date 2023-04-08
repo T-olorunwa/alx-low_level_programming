996682b92363eea6e7ff640c3aa9da33fc887d51
Qudus <Aqolorunwa@gmail.com>
2023-04-08 07:01:05 -0700

    Print factorial of an integer

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,13 +9,11 @@
 
 int factorial(int n)
 {
-	int factorial(n);
 
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else 
-		factorial(n) = n * factorial(n - 1);
-	return (factorial(n));
+	else
+		return  (n * factorial(n - 1));
 }
