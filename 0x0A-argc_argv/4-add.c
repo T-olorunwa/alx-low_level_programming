c2b889a6954331a2461d11ca0c4f69e43408bb68
Qudus <Aqolorunwa@gmail.com>
2023-04-10 16:02:38 -0700

    Modified 1-args.c one last time

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,18 +17,18 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 		printf("0\n");
 
-	for (i = 0; i <= 9; i++)
-	{
-		if(argc != i)
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-
 	for (j = 1; j <= argc; j++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if(argc != i)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		add += atoi(argv[j]);
-
+	}
 	printf("%d\n", add);
 
 	return (0);
