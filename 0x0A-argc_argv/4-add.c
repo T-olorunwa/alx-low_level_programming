3fc44f898721f7f08a17547b2709683827e15fb3
Qudus <Aqolorunwa@gmail.com>
2023-04-10 15:41:18 -0700

    Prints the sum of arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Add and prints positive integer
+ * @argc: Argument counter
+ * @argv: Pointer to an array of strings
+ *
+ * Return: zero (0), if no number is passed
+ *	one (1) and print "Error" if number contains symbols
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (i = 0; i <= 9; i++)
+	{
+		if(argc != i)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	for (j = 1; j <= argc; j++)
+		add += atoi(argv[j]);
+
+	printf("%d\n", add);
+
+	return (0);
+}
